1c08c69663010f6b1dcb63057ef8614a56be3f44
Anthony Levings <sketchytech@gylphi.co.uk>
2014-08-08 10:44:59 +0100

    Updated following a StackOverflow solution
    Matt Neuburg posted this solution - http://stackoverflow.com/a/24916777/1694526 - to the Range issue and the extension works much better with it because it handles Unicode scalars now.

--- extension.swift
+++ extension.swift
@@ -1,10 +1,7 @@
 extension String {
 
     func substringWithNSRange(range:NSRange)->String {
-        
-        let begin = advance(self.startIndex, range.location), finish = advance(self.endIndex, range.location+range.length-countElements(self))
-        
-        
-        return self.substringWithRange(Range(start:begin, end:finish))
+        let str = self as NSString
+        return str.substringWithRange(range)
     }
 }
